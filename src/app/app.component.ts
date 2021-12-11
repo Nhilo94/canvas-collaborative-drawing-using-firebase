6a54c5c96c2b06d37edebfd851b246ea1250c72b
Fanilo RAKOTOVAO <fanilorakoto.94@gmail.com>
2021-12-11 22:02:24 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pencil-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'pencil-canvas';
+}
