472e3e56b9cbf89fbc0226751c6b37de5ea26c28
Fanilo RAKOTOVAO <fanilorakoto.94@gmail.com>
2021-12-14 23:31:22 +0300

    Fanilo | prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyCCkadi6twWYAkmDfgVaP4XC9SWjl4BmxQ',
-    authDomain: 'p-canvas.firebaseapp.com',
-    projectId: 'p-canvas',
-    storageBucket: 'p-canvas.appspot.com',
-    messagingSenderId: '802062892223',
-    appId: '1:802062892223:web:f55e646994087215591a0a',
-    measurementId: 'G-CHCWHNV0X6',
+    apiKey: "AIzaSyCCkadi6twWYAkmDfgVaP4XC9SWjl4BmxQ",
+    authDomain: "p-canvas.firebaseapp.com",
+    databaseURL: "https://p-canvas-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "p-canvas",
+    storageBucket: "p-canvas.appspot.com",
+    messagingSenderId: "802062892223",
+    appId: "1:802062892223:web:f55e646994087215591a0a",
+    measurementId: "G-CHCWHNV0X6"
   },
 };
