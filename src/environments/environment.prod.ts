d8b0d8865fed566ae6c9773660041a3725f4f6f0
Fanilo RAKOTOVAO <fanilorakoto.94@gmail.com>
2021-12-14 16:21:07 +0300

    Fanilo | env fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyBfuJFr_989VgmC9-619fl40W9WX0rlGGs',
-    authDomain: 'vuejsp1.firebaseapp.com',
-    databaseURL: 'https://vuejsp1.firebaseio.com/',
-    projectId: 'vuejsp1',
-    storageBucket: 'vuejsp1.appspot.com',
-    messagingSenderId: '406536360776',
-    appId: '1:406536360776:web:e5d156850ddd129de2d97d',
+    apiKey: 'AIzaSyCCkadi6twWYAkmDfgVaP4XC9SWjl4BmxQ',
+    authDomain: 'p-canvas.firebaseapp.com',
+    projectId: 'p-canvas',
+    storageBucket: 'p-canvas.appspot.com',
+    messagingSenderId: '802062892223',
+    appId: '1:802062892223:web:f55e646994087215591a0a',
+    measurementId: 'G-CHCWHNV0X6',
   },
 };
